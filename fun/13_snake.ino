60850eceec8c15cc1e503eb32d98896ce125802c
Matías Rodlo <m@hablaqui.com>
2024-07-24 23:56:13 -0400

    fun project folder

--- fun/13_snake.ino
+++ fun/13_snake.ino
@@ -27,8 +27,7 @@
 #include "WiFiS3.h"
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = Lake Placid Lodge;        // your network SSID (name)
 int keyIndex = 0;                 // your network key index number (needed only for WEP)
 
 int led =  LED_BUILTIN;
